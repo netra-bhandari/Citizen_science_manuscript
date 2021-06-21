2fe5d7f41a86565da8964cfe6ef5eb16977ec558
Diana Bowler <diana.bowler@idiv.de>
2021-06-21 17:24:57 +0200

    begin file to contain all useful functions

---
+++ helper_functions.R
@@ -0,0 +1,38 @@
+#helper_functions
+
+#replace ordinal scale to a numeric scale
+
+ordinal_fn <- function(x){
+  x <- x %>%  mutate_all(funs(case_when(. == "sehr oft" ~ "5" ,
+                                        . ==  "oft" ~ "4" ,
+                                        . ==  "manchmal" ~ "3" ,
+                                        . ==  "selten" ~ "2",
+                                        . ==  "nie" ~ "1",
+                                        . ==  "wei? nicht" ~ "0",
+                                        . == "sehr wichtig" ~ "5",
+                                        . == "wichtig" ~ "4",
+                                        . == "mäßig wichtig" ~ "3",
+                                        . == "wenig wichtig" ~ "2",
+                                        . == "überhaupt nicht wichtig" ~ "1",
+                                        . ==   "alle"~ "5",
+                                        . ==   "die meisten" ~ "4",
+                                        . ==   "einige" ~ "3",
+                                        . ==   "wenige" ~ "2",
+                                        . ==   "keine" ~ "1",
+                                        . ==  "?berhaupt nicht wahrscheinlich" ~ "1",
+                                        . ==  "wenig wahrscheinlich" ~ "2" ,
+                                        . ==  "m??ig wahrscheinlich" ~ "3" ,
+                                        . ==  "ziemlich wahrscheinlich" ~ "4",
+                                        . ==  "sehr wahrscheinlich" ~ "5",
+                                        . ==  "Ja" ~ "1",
+                                        . ==   "Nein" ~ "0"
+  )))
+  
+  return(x)
+  
+}
+
+
+combineTaxa <- function(x){
+  x[x %in% c("Bienen","Käfer","Libellen","Schmetterlinge")] <- "Insects"
+}
