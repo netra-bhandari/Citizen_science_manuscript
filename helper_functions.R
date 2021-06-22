c9dfaafb3b6712ef4c72abc42d33c0d822de5bbd
Diana Bowler <diana.bowler@idiv.de>
2021-06-22 15:20:08 +0200

    fit german accents

--- helper_functions.R
+++ helper_functions.R
@@ -8,20 +8,20 @@ ordinal_fn <- function(x){
                                         . ==  "manchmal" ~ "3" ,
                                         . ==  "selten" ~ "2",
                                         . ==  "nie" ~ "1",
-                                        . ==  "wei? nicht" ~ "0",
+                                        . ==  "weiß nicht" ~ "0",
                                         . == "sehr wichtig" ~ "5",
                                         . == "wichtig" ~ "4",
                                         . == "mäßig wichtig" ~ "3",
                                         . == "wenig wichtig" ~ "2",
                                         . == "überhaupt nicht wichtig" ~ "1",
-                                        . ==   "alle"~ "5",
+                                        . ==   "alle" ~ "5",
                                         . ==   "die meisten" ~ "4",
                                         . ==   "einige" ~ "3",
                                         . ==   "wenige" ~ "2",
                                         . ==   "keine" ~ "1",
-                                        . ==  "?berhaupt nicht wahrscheinlich" ~ "1",
+                                        . ==  "überhaupt nicht wahrscheinlich" ~ "1",
                                         . ==  "wenig wahrscheinlich" ~ "2" ,
-                                        . ==  "m??ig wahrscheinlich" ~ "3" ,
+                                        . ==  "mäßig wahrscheinlich" ~ "3" ,
                                         . ==  "ziemlich wahrscheinlich" ~ "4",
                                         . ==  "sehr wahrscheinlich" ~ "5",
                                         . ==  "Ja" ~ "1",
@@ -36,3 +36,12 @@ ordinal_fn <- function(x){
 combineTaxa <- function(x){
   x[x %in% c("Bienen","Käfer","Libellen","Schmetterlinge")] <- "Insects"
 }
+
+
+# funtion to  wrap the long questions for a tidy representation in plots
+
+swr = function(string, nwrap=20) {
+  paste(strwrap(string, width=nwrap), collapse="\n")
+}
+
+swr = Vectorize(swr)
