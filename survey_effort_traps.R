8887f3e5196818a47478acc7a33880382551d466
netra-bhandari <54842248+netra-bhandari@users.noreply.github.com>
2021-07-02 15:14:24 +0530

    Update survey_effort_traps.R
    changed x axis label

--- survey_effort_traps.R
+++ survey_effort_traps.R
@@ -36,12 +36,14 @@ sample_data$Taxa <-   str_replace_all(sample_data$Taxa,
 
 
 
-ggplot(subset(sample_data,!is.na(activeMins)))+
+activity_plot <- ggplot(subset(sample_data,!is.na(activeMins)))+
   geom_density_ridges(aes(y=Taxa, x=activeMins, fill=Taxa))+
   scale_fill_brewer(type="qual", palette="Set3")+ #changed pallete from "Accent" to "Set 3" to add the 9th color in the plot
   theme_few()+
-  theme(legend.position="none")
+  theme(legend.position="none")+
+  xlab("Activity duration (minutes)")
   
+ggsave(activity_plot, filename = "plots/fig9_new.png")
 
 sample_data %>%
   dplyr::group_by(Taxa) %>%
